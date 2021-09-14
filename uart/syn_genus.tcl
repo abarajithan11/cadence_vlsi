5fffbcec663582e4f4a7e5232214f4450e3530d4
aba <abarajithan07@gmail.com>
2021-09-14 13:41:39 +0530

    Fix syn.tcl

--- uart/syn_genus.tcl
+++ uart/syn_genus.tcl
@@ -6,12 +6,12 @@ set_db library saed32hvt_ss0p7v125c.lib
 read_hdl [glob 1_rtl/*.v]
 
 # 3. Elaborate the top module
-elaborate uart_top > log/2.elaborate.log
+elaborate uart_top
 
 # 4. Check Design
 check_design > log/3.check_design.log
 
-uniquify
+uniquify uart_top
 
 # 5. Set constraints
 source constraints.tcl
@@ -20,11 +20,11 @@ source constraints.tcl
 synthesize -to_mapped -effort h
 
 # 7. Write netlist
-write -mapped > syn_out/$top.v
-write_sdc syn_out/$top.sdc
+write -mapped > 2_syn_out/$top.v
+write_sdc > 2_syn_out/$top.sdc
 
 # 8. Reports
 report_area > report/area.log
-report_timing  -nworst 10 > ../report/timing.log
+report_timing  -nworst 10 > report/timing.log
 report_constraint > report/constraint.log
-report_port  > report/ports_final.log
+report_port * > report/ports_final.log
