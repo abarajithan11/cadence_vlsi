3b7ae44b20bc9514e61c63edc85402afe14cdbf8
aba <abarajithan07@gmail.com>
2021-09-20 15:04:19 +0530

    Update tcl for cadence pdk

--- basics/syn_genus.tcl
+++ basics/syn_genus.tcl
@@ -1,5 +1,7 @@
-set_db init_lib_search_path [list ../libs]
-set_db library saed32hvt_ss0p7v125c.lib
+set_db init_lib_search_path [list ../libs/gsclib045/lef ../libs/gsclib045/timing ../libs/gsclib045/qrc/qx]
+set_db library fast_vdd1v0_basicCells.lib
+set_db lef_library {gsclib045_tech.lef gsclib045_macro.lef gsclib045_multibitsDFF.lef}
+set_db qrc_tech_file gpdk045.tch
 
 read_hdl -sv ${DESIGN}.sv
 elaborate
